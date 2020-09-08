b8905c1d71354abd2b9492b61c8b8fe8cec73ec5
techiefrankie <techiefrankie@gmail.com>
2020-09-08 04:22:35 +0100

    Fix bug

--- foodie/webapp/urls.py
+++ foodie/webapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import upload_photo, recommend
+from .views import recommend
 
 urlpatterns = [
-    path('upload_photo/', upload_photo, name='upload_photo'),
     path('recommend/', recommend, name='recommend'),
 ]
